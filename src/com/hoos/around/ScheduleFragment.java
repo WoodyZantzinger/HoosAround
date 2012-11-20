5d23b53639fe5cfb1536387a1647a1b21e3288b8
Woody Zantzinger <wz2ae@virginia.edu>
2012-11-20 10:00:12 -0500

    Added 'Add Class' button

--- src/com/hoos/around/ScheduleFragment.java
+++ src/com/hoos/around/ScheduleFragment.java
@@ -2,6 +2,7 @@ package com.hoos.around;
 
 import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -10,6 +11,7 @@ import org.json.JSONObject;
 import com.hoos.around.ImageThreadLoader.ImageLoadedListener;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
+import android.app.Dialog;
 import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -22,22 +24,29 @@ import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class ScheduleFragment extends Fragment{
 	
-	private ArrayList<Class> ClassList = new ArrayList<Class>();
+	private List<Class> ClassList = new ArrayList<Class>();
+	
 	private TextView detail_header;
 	private TextView detail_time;
 	private TextView detail_location;
-	private ScheduleAdapter scheduleAdapter;
+	private Button new_class;
 	private ListView ClassListView;
+	
+	private ScheduleAdapter scheduleAdapter;
+	
     private ImageThreadLoader imageLoader = new ImageThreadLoader();
     
     private Class selected_class;
@@ -69,6 +78,44 @@ public class ScheduleFragment extends Fragment{
 		detail_header.setText(class_Detail.course_mnem);
 		detail_location.setText(String.valueOf(class_Detail.location_id));
 		detail_time.setText(class_Detail.course_start + " till " + class_Detail.course_end);
+	}
+	
+	public void LoadClasses() {
+		RestClient.get("courses/view", null, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(JSONArray classes) {
+                // Populate Class List
+				try {
+					
+					for(int x = 0; x < classes.length(); x++) {
+						Class temp = new Class();
+						JSONObject JSONClasses = (JSONObject)classes.get(x);
+						temp.course_id = JSONClasses.getJSONObject("Course").getInt("course_id");
+						temp.course_start = JSONClasses.getJSONObject("Course").getString("course_start");
+						temp.course_end = JSONClasses.getJSONObject("Course").getString("course_end");
+						temp.course_mnem = JSONClasses.getJSONObject("Course").getString("course_mnem");
+						temp.location_id = JSONClasses.getJSONObject("Course").getInt("location_id");
+						ClassList.add(temp);
+					}
+					
+					//MAKE BUTTON VISISLBE
+					new_class.setClickable(true);
+					
+					Log.d("JSON", "MSG RECIEVED");
+					
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					Log.d("JSON", e.getMessage());
+				}
+            }
+            
+            @Override
+            public void onFailure(Throwable e, String response) {
+				Log.d("JSON", response);
+				Log.d("JSON", RestClient.getAbsoluteUrl("courses/view/"));
+            }
+            
+        });	
 	}
 	
 	public void LoadSchedule(User user) {
@@ -139,6 +186,34 @@ public class ScheduleFragment extends Fragment{
 				   adapter.setSelection(position);
 			   } 
 			});
+		
+		new_class = (Button)view.findViewById(R.id.scheduleListAdd);
+		new_class.setClickable(false);
+		LoadClasses();
+		new_class.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				final Dialog addDialog = new Dialog(arg0.getContext(), R.style.CustomDialogTheme);
+				addDialog.setContentView(R.layout.add_class_dialog);
+				
+				Spinner class_spinner = (Spinner) addDialog.findViewById(R.id.class_spinner);
+				ArrayAdapter<Class> dataAdapter = new ArrayAdapter<Class>(arg0.getContext(),
+					android.R.layout.simple_spinner_item, ClassList);
+				dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				class_spinner.setAdapter(dataAdapter);
+				
+				addDialog.show();
+				
+				Button close_btn = (Button) addDialog.findViewById(R.id.submit_button);
+				close_btn.setOnClickListener(new View.OnClickListener() {
+				    public void onClick(View v) {
+				    	addDialog.dismiss();
+				    }
+				});
+				
+			}
+		});
 		
 		detail_header = (TextView)view.findViewById(R.id.scheduleDetailHeader);
 		detail_time = (TextView)view.findViewById(R.id.scheduleDetailTime);
