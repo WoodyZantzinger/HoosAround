d6444b3ea632c6295727514859a402dee34c68d3
awnicho <Andrew.Nicholson@noblis.org>
2012-11-21 23:10:08 -0500

    added help message for login bug

--- src/com/hoos/around/Login.java
+++ src/com/hoos/around/Login.java
@@ -28,7 +28,7 @@ public class Login extends Activity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
 
         facebook.authorize(this, new DialogListener() {
             @Override
